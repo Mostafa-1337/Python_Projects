3e7f58eeb9cedbb87855b2ec2a5840c67f371472
Mostafa-1337 <mostafafwork@gmail.com>
2026-07-26 07:21:22 +0300

    Add files via upload

---
+++ 08_amazon_demo/main.py
@@ -0,0 +1,12 @@
+import asyncio
+from exporters.excel_exporter import save_to_excel
+from exporters.json_exporter import save_to_json
+from scraper.scraper import main
+
+def main_file():
+    scrape_data = asyncio.run(main())
+    save_to_json(scrape_data)
+    save_to_excel(scrape_data)
+
+if __name__ == "__main__":
+    main_file()
